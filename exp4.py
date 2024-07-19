396270c107e8dbf07d38c99701bc518cffe2f808
AKSHAT <akshatgoel700@gmail.com>
2024-07-19 16:38:18 +0530

    final

---
+++ exp4.py
@@ -0,0 +1,7 @@
+import nltk
+from nltk.tokenize import word_tokenize
+
+from nltk.tokenize import word_tokenize
+text = "NLTK is a leading platform for building Python programs to work with human language data."
+tokens = word_tokenize(text)
+print(tokens)
